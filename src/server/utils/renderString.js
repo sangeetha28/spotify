7c98d8b6d40d149d369f9dbc42266c49ea71937f
sangeetha rajasekaran <sangeetharajasekaran@notonthehighstreet.com>
2019-03-26 19:40:38 +0000

    add google font and fix overlay

--- src/server/utils/renderString.js
+++ src/server/utils/renderString.js
@@ -4,13 +4,13 @@ import React from "react";
 import { StaticRouter } from "react-router-dom";
 import { renderRoutes } from "react-router-config";
 import { Provider } from "react-redux";
-import Routes from "../Routes"
+import Routes from "../Routes";
 
-export const renderInitialHTML = (req,store) => {
+export const renderInitialHTML = (req, store) => {
   const content = renderToString(
     <Provider store={store}>
       <StaticRouter location={req.path} context={{}}>
-      <div>{renderRoutes(Routes)}</div>
+        <div>{renderRoutes(Routes)}</div>
       </StaticRouter>
     </Provider>
   );
@@ -19,6 +19,7 @@ export const renderInitialHTML = (req,store) => {
     <head>
     <title>Spotify App</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     <link rel="stylesheet" href="styles.css" />
     
     </head>
