cd89065b4d5b4c3aa01ca905e7373b445ddc67bb
sangeetha rajasekaran <sangeetharajasekaran@notonthehighstreet.com>
2019-03-26 23:36:18 +0000

    add Loader Component

---
+++ src/client/components/Loader.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default () => (
+  <div>
+    <div className="ui active dimmer">
+      <div className="ui huge text loader">Loading</div>
+    </div>
+    <p />
+    <p />
+    <p />
+  </div>
+);
